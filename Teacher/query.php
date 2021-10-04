eda1246df21b8f59c9fc7b450fdf760f3fcf9884
Cheng-Man-Hei <60951363+Cheng-Man-Hei@users.noreply.github.com>
2021-10-04 22:57:36 +0800

    Create query.php

---
+++ Teacher/query.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Query</title>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<script src="/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+<style>
+	html,body {
+		height: 100%;
+	}	
+</style>
+</head>
+
+<body>
+	<div class="container h-100 row align-items-center">
+	<div class="col-md-4 offset-6" >
+	<h1 class="mb-5">Student Marks Query System</h1>
+	<form action="query_submit.php" method="POST">
+	<input type="text" class="query" name="query" id="query" placeholder="Student Username">
+	<button class="btn btn-danger" type="submit">Search</button>
+		</form>
+		</div>
+		</div>
+</body>
+</html>
